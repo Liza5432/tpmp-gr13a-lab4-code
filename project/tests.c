524199c6923a1b2e4b56386659076ffd38d45c66
Alina-zh-123 <6817076@mail.ru>
2026-03-25 15:17:54 +0300

    Update tests.c

--- project/tests.c
+++ project/tests.c
@@ -2,40 +2,37 @@
 #include <stdio.h>
 #include "../includes/db.h"
 
+int Get_Flower_Price(sqlite3 *db, int id, double *price);
+
 void test_database_logic() {
     sqlite3 *db;
     int rc;
-    
+
     rc = sqlite3_open(":memory:", &db);
     assert(rc == SQLITE_OK);
-    assert(db != NULL);
-    printf("Test 0: Database open - OK\n");
-    
-    rc = Create_Tables(db);
-    assert(rc == SQLITE_OK);
-    printf("Test 1: Create_Tables - OK\n");
-    
-    rc = Insert_Flower(db, "Test", "Sort", 10.0);
-    assert(rc == SQLITE_OK);
-    
-    double price;
+    printf("Test 0: DB open OK\n");
+
+    Create_Tables(db);
+    printf("Test 1: Create_Tables OK\n");
+
+    Insert_Flower(db, "Test", "Sort", 10.0);
+
+    double price = 0;
     rc = Get_Flower_Price(db, 1, &price);
     assert(rc == SQLITE_OK);
     assert(price == 10.0);
-    printf("Test 2: Insert_Flower - OK\n");
-    
-    rc = Safe_Price_Update(db, 1, 15.0);
-    assert(rc == SQLITE_OK);
-    
+    printf("Test 2: Insert_Flower OK\n");
+
+    Safe_Price_Update(db, 1, 15.0);
+
     rc = Get_Flower_Price(db, 1, &price);
     assert(rc == SQLITE_OK);
     assert(price == 15.0);
-    printf("Test 3: Safe_Price_Update - OK\n");
-    
-    rc = Delete_Order_By_Id(db, 999);
-    assert(rc == SQLITE_OK);
-    printf("Test 4: Delete_Order - OK\n");
-    
+    printf("Test 3: Safe_Price_Update OK\n");
+
+    Delete_Order_By_Id(db, 999);
+    printf("Test 4: Delete_Order OK\n");
+
     sqlite3_close(db);
 }
 
